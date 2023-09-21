f2b8bc21fe0d576be26b5560b4f120d336ac80ac
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-09-21 16:43:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 //your JS code here. If required.
 let info=document.getElementById("info");
-info.innerText=` You are using ${navigator.userAgent} ${navigator.appName} ${navigator.appVersion}.`
+info.innerText=` You are using ${navigator.userAgent} ${navigator.appName} ${navigator.appVersion} .`
